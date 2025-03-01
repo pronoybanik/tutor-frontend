8aebc5e4b488c30316bb921d68f985e1121861e7
PRONOY <pronoybanik82@gmail.com>
2025-03-01 12:25:01 +0600

    create some route and fiels

---
+++ src/app/(withDashboardLayout)/layout.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <>
+      <main className="min-h-screen">{children}</main>
+    </>
+  );
+};
+
+export default DashboardLayout;
